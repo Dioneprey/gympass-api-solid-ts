4e68829353e96b2838ee18696f0bd6c2b545e5f1
Dionatan <dionatan.1751@gmail.com>
2023-07-09 23:46:36 -0300

    controller, use case e repository de users

---
+++ src/http/controllers/register.ts
@@ -0,0 +1,34 @@
+import { FastifyRequest, FastifyReply } from 'fastify'
+import { PrismaUsersRepository } from 'src/repositories/prisma/prisma-users-repository'
+import { UserAlreadyExistsError } from 'src/use-cases/errors/user-already-exist-error'
+import { RegisterUseCase } from 'src/use-cases/register'
+import { z } from 'zod'
+
+export async function register(request: FastifyRequest, reply: FastifyReply) {
+  const registerBodySchema = z.object({
+    name: z.string(),
+    email: z.string().email(),
+    password: z.string().min(6),
+  })
+
+  const { name, email, password } = registerBodySchema.parse(request.body)
+
+  try {
+    const usersRepository = new PrismaUsersRepository()
+    const registerUseCase = new RegisterUseCase(usersRepository)
+
+    await registerUseCase.execute({
+      name,
+      email,
+      password,
+    })
+  } catch (error) {
+    if (error instanceof UserAlreadyExistsError) {
+      return reply.status(409).send({ message: error.message })
+    }
+
+    throw error
+  }
+
+  return reply.status(201).send()
+}
