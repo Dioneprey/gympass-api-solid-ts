8ea2c47520d536e98406640894f76a9ec3b960fd
Dionatan <dionatanonliveira@cidadaoon.com>
2023-07-13 16:59:57 -0300

    verificando data de checkin

--- src/repositories/check-ins-repository.ts
+++ src/repositories/check-ins-repository.ts
@@ -2,4 +2,5 @@ import { CheckIn, Prisma } from '@prisma/client'
 
 export interface CheckInsRepository {
   create(date: Prisma.CheckInUncheckedCreateInput): Promise<CheckIn>
+  findByUserIdOnDate(userId: string, date: Date): Promise<CheckIn | null>
 }
